0dd10d645614a4b8366cd13cc896e36b8449c81e
Mark Dingemanse <dingemansemark@gmail.com>
2016-09-20 21:09:25 +0000

    added basic client functionality oauth laravel passport

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,34 @@
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
 Route::get('/', function () {
-    return view('welcome');
+
+    $query = http_build_query([
+        'client_id' => '4',
+        'redirect_uri' => 'http://deltaclient.dev/callback',
+        'response_type' => 'code',
+        'scope' => ''
+    ]);
+
+    return redirect('http://deltasecure.dev/oauth/authorize?'.$query);
+});
+
+Route::get('/callback', function (Illuminate\Http\Request $request) {
+    $http = new \GuzzleHttp\Client;
+
+    $response = $http->post('http://deltasecure.dev/oauth/token', [
+        'form_params' => [
+            'client_id' => '4',
+            'client_secret' => 'fV0AJ649OYdjG1JpO4KJr42LdEmLDKal78FGA1L9',
+            'grant_type' => 'authorization_code',
+            'redirect_uri' => 'http://deltaclient.dev/callback',
+            'code' => $request->code,
+        ],
+    ]);
+    return json_decode((string) $response->getBody(), true);
 });
